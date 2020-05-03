57b244ebd7a7f6667096125367880a4235845298
John Young <johnfyoung22@yahoo.com>
2020-05-02 19:45:41 -0500

    First commit. Uploading existing program.

---
+++ ArrayFunGUI/Form1 (1).cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayFunGUI
+{
+    public partial class frmArrayFunGUI : Form
+    {
+        //Declare and initialize global constants
+        const int ARRAYSIZE = 10;
+        const int MINVAL = 1;
+        const int MAXVAL = 101;
+
+        //Declare and initialize global variables
+        static int[] numArray = new int[ARRAYSIZE];
+        static int[] origArray = new int[ARRAYSIZE];
+
+        public frmArrayFunGUI()
+        {
+            InitializeComponent();
+        }
+
+        //*******************************************
+
+        private void btnDisplayArray_Click(object sender, EventArgs e)
+        {
+            generateArray();
+            displayArray();
+        }
+
+        //*******************************************
+
+        private void btnLowest_Click(object sender, EventArgs e)
+        {
+            showLowest();
+        }
+
+        //*******************************************
+
+        private void btnHighest_Click(object sender, EventArgs e)
+        {
+            showHighest();
+        }
+
+        //*******************************************
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            showSum();
+        }
+
+        //*******************************************
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            showAvg();
+        }
+
+        //*******************************************
+
+        private void btnAscending_Click(object sender, EventArgs e)
+        {
+            showAsc();
+            displayArray();
+        }
+
+        //*******************************************
+
+        private void btnDescending_Click(object sender, EventArgs e)
+        {
+            showDesc();
+            displayArray();
+        }
+
+        //*******************************************
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            arraySearch();
+        }
+
+        //*******************************************
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearListBox();
+        }
+
+        //*******************************************
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            displayMessageBox("Press Display Array to generate a new array."+
+                              "\nPress Display Lowest to show a message box showing the lowest item in the array." +
+                              "\nPress Display Highest to show a message box showing the highest item in the array." +
+                              "\nPress Display Sum to show the sum of all the elements in the array" +
+                              "\nPress Display Avg to show the average of all the elements in the array" +
+                              "\nPress Sort Ascending to show the array elements in ascending order." +
+                              "\nPress Sort Descending to show the array elements in descending order." +
+                              "\nPress Binary Search to search for the input in the nearby textbox." +
+                              "\nPress Clear to empty the list box." +
+                              "\nPress Exit to close the program",
+                              "HELP");
+        }
+
+        //*******************************************
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            exitApplication();
+        }
+
+        //*******************************************
+
+        private void displayArray()
+        {
+            clearListBox();
+            for (int i = 0; i < ARRAYSIZE; ++i)
+            {
+                listboxDisplay.Items.Add(numArray[i].ToString());
+            }
+        }
+
+        //*******************************************
+
+        static void generateArray()
+        {
+            //Generates a new array, 10 numbers between 1 and 100
+            Random randNum = new Random();
+            for (int i = 0; i < ARRAYSIZE; i++)
+            {
+                numArray[i] = randNum.Next(MINVAL, MAXVAL);
+                origArray[i] = randNum.Next(MINVAL, MAXVAL);
+            }
+        }
+
+        //*******************************************
+
+        private void showLowest()
+        {
+            int lowest = 0;
+
+            Array.Sort(numArray);
+            lowest = numArray[0];
+
+            displayMessageBox("The lowest number in the array is " + lowest,
+                              "LOWEST ELEMENT");
+        }
+
+        //*******************************************
+
+        private void showHighest()
+        {
+            int highest = 0;
+
+            Array.Sort(numArray);
+            highest = numArray[9];
+
+            displayMessageBox("The highest number in the array is " + highest,
+                              "Highest ELEMENT");
+        }
+
+        //*******************************************
+
+        private int showSum()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < ARRAYSIZE; ++i)
+            {
+                sum += numArray[i];
+            }
+            
+            displayMessageBox("The sum of the array's elements is " + sum,
+                              "ARRAY SUM");
+            return sum;
+        }
+
+        //*******************************************
+
+        private void showAvg()
+        {
+            int avg = showSum() / ARRAYSIZE;
+
+            displayMessageBox("The average of the array's elements is " + avg,
+                              "ARRAY AVERAGE");
+        }
+
+        //*******************************************
+
+        private void showAsc()
+        {
+            Array.Sort(numArray);
+        }
+
+        //*******************************************
+
+        private void showDesc()
+        {
+            Array.Sort(numArray);
+            Array.Reverse(numArray);
+        }
+
+        //*******************************************
+
+        private void arraySearch()
+        {
+            int myIndex = 0;
+            int searchItem = 0;
+
+            Array.Sort(numArray);
+            if (isNumeric(txtSearch.Text))
+            {
+                searchItem = Convert.ToInt32(txtSearch.Text);
+                if((searchItem > 0) && (searchItem < 101))
+                {
+                    myIndex = Array.BinarySearch(numArray, searchItem);
+                    if (myIndex < 0)
+                    {
+                        displayErrorMessage("Input was not found in the array.",
+                                              "SEARCH ERROR");
+                    }
+                    else
+                    {
+                        myIndex += 1;
+                        displayMessageBox(searchItem + " is item " + myIndex + " in the array.",
+                                          "ARRAY SEARCH");
+                    }
+                }
+                else
+                {
+                    displayErrorMessage("Input must be a number between 1 and 100",
+                                      "SEARCH ERROR");
+                }
+            }
+            else
+            {
+                displayErrorMessage("Input must be a number between 1 and 100",
+                                      "SEARCH ERROR");
+            }
+        }
+
+
+
+        private void clearListBox()
+        {
+            listboxDisplay.Items.Clear();
+        }
+
+        //*******************************************
+
+        static bool isNumeric(String input)
+        {
+            double test;
+            return double.TryParse(input, out test);  //Return true if the input can converted to an double
+        }
+
+        //*******************************************
+
+        private void displayMessageBox(string msg, string title)
+        {
+            MessageBox.Show(msg,
+                            title,
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        //*******************************************
+
+        private void displayErrorMessage(string msg, string title)
+        {
+            MessageBox.Show(msg,
+                            title,
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+        }
+
+        //*******************************************
+
+        private void exitApplication()
+        {
+            if (MessageBox.Show("Exit Program Now?",
+                                "EXIT PROGRAM???",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Question) ==
+                                DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
---
+++ ArrayFunGUI/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ArrayFunGUI
+{
+    partial class frmArrayFunGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listboxDisplay = new System.Windows.Forms.ListBox();
+            this.btnDisplayArray = new System.Windows.Forms.Button();
+            this.btnLowest = new System.Windows.Forms.Button();
+            this.btnHighest = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDescending = new System.Windows.Forms.Button();
+            this.btnAscending = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listboxDisplay
+            // 
+            this.listboxDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listboxDisplay.FormattingEnabled = true;
+            this.listboxDisplay.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.listboxDisplay.ItemHeight = 31;
+            this.listboxDisplay.Location = new System.Drawing.Point(103, 68);
+            this.listboxDisplay.Name = "listboxDisplay";
+            this.listboxDisplay.Size = new System.Drawing.Size(56, 314);
+            this.listboxDisplay.TabIndex = 0;
+            this.listboxDisplay.TabStop = false;
+            // 
+            // btnDisplayArray
+            // 
+            this.btnDisplayArray.Location = new System.Drawing.Point(376, 46);
+            this.btnDisplayArray.Name = "btnDisplayArray";
+            this.btnDisplayArray.Size = new System.Drawing.Size(119, 50);
+            this.btnDisplayArray.TabIndex = 0;
+            this.btnDisplayArray.Text = "Display Array";
+            this.btnDisplayArray.UseVisualStyleBackColor = true;
+            this.btnDisplayArray.Click += new System.EventHandler(this.btnDisplayArray_Click);
+            // 
+            // btnLowest
+            // 
+            this.btnLowest.Location = new System.Drawing.Point(376, 122);
+            this.btnLowest.Name = "btnLowest";
+            this.btnLowest.Size = new System.Drawing.Size(119, 50);
+            this.btnLowest.TabIndex = 1;
+            this.btnLowest.Text = "Display Lowest";
+            this.btnLowest.UseVisualStyleBackColor = true;
+            this.btnLowest.Click += new System.EventHandler(this.btnLowest_Click);
+            // 
+            // btnHighest
+            // 
+            this.btnHighest.Location = new System.Drawing.Point(376, 196);
+            this.btnHighest.Name = "btnHighest";
+            this.btnHighest.Size = new System.Drawing.Size(119, 50);
+            this.btnHighest.TabIndex = 2;
+            this.btnHighest.Text = "Display Highest";
+            this.btnHighest.UseVisualStyleBackColor = true;
+            this.btnHighest.Click += new System.EventHandler(this.btnHighest_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(376, 272);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(119, 50);
+            this.btnSum.TabIndex = 3;
+            this.btnSum.Text = "Display Sum";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(376, 347);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(119, 50);
+            this.btnAverage.TabIndex = 4;
+            this.btnAverage.Text = "Display Avg";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(482, 433);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(119, 50);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(574, 272);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(119, 50);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear Listbox";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(574, 196);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(119, 50);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Binary Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDescending
+            // 
+            this.btnDescending.Location = new System.Drawing.Point(574, 122);
+            this.btnDescending.Name = "btnDescending";
+            this.btnDescending.Size = new System.Drawing.Size(119, 50);
+            this.btnDescending.TabIndex = 6;
+            this.btnDescending.Text = "Display Descending";
+            this.btnDescending.UseVisualStyleBackColor = true;
+            this.btnDescending.Click += new System.EventHandler(this.btnDescending_Click);
+            // 
+            // btnAscending
+            // 
+            this.btnAscending.Location = new System.Drawing.Point(574, 46);
+            this.btnAscending.Name = "btnAscending";
+            this.btnAscending.Size = new System.Drawing.Size(119, 50);
+            this.btnAscending.TabIndex = 5;
+            this.btnAscending.Text = "Display Ascending";
+            this.btnAscending.UseVisualStyleBackColor = true;
+            this.btnAscending.Click += new System.EventHandler(this.btnAscending_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(574, 347);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(119, 50);
+            this.btnHelp.TabIndex = 10;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(727, 212);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 8;
+            // 
+            // frmArrayFunGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(839, 495);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDescending);
+            this.Controls.Add(this.btnAscending);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.btnHighest);
+            this.Controls.Add(this.btnLowest);
+            this.Controls.Add(this.btnDisplayArray);
+            this.Controls.Add(this.listboxDisplay);
+            this.Name = "frmArrayFunGUI";
+            this.Text = "Array Fun GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listboxDisplay;
+        private System.Windows.Forms.Button btnDisplayArray;
+        private System.Windows.Forms.Button btnLowest;
+        private System.Windows.Forms.Button btnHighest;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDescending;
+        private System.Windows.Forms.Button btnAscending;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
